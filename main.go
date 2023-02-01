3410e8ff25a493c809a0f432690ffbb3f4487a8a
hamonangann <bornyto.hamonangan@ui.ac.id>
2023-02-01 23:14:14 +0700

    add form

--- main.go
+++ main.go
@@ -12,6 +12,41 @@ type Person struct {
 }
 
 func main() {
+	http.HandleFunc("/message", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "GET" {
+			tmpl := template.Must(template.New("index").ParseFiles(
+				"views/form.html",
+				"views/_header.html",
+			))
+			if err := tmpl.Execute(w, nil); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+			return
+		}
+		http.Error(w, "", http.StatusBadRequest)
+	})
+	http.HandleFunc("/message-delivered", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "POST" {
+			tmpl := template.Must(template.New("result").ParseFiles(
+				"views/form.html",
+				"views/_header.html"))
+			if err := r.ParseForm(); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+
+			name := r.FormValue("name")
+			message := r.FormValue("message")
+
+			data := map[string]string{"name": name, "message": message}
+
+			if err := tmpl.Execute(w, data); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+			return
+		}
+		http.Error(w, "", http.StatusBadRequest)
+	})
+
 	http.HandleFunc("/index", func(w http.ResponseWriter, r *http.Request) {
 		data := Person{Name: "abdulbari"}
 		tmpl := template.Must(template.ParseFiles(
@@ -38,6 +73,10 @@ func main() {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	})
+
+	http.Handle("/static/",
+		http.StripPrefix("/static/",
+			http.FileServer(http.Dir("assets"))))
 
 	fmt.Println("server started at localhost:9000")
 	http.ListenAndServe(":9000", nil)
